77180154096cef454083a64e974e80e0b329c497
Phuc Thinh <thinh29112001@gmail.com>
2020-09-19 09:40:37 +0700

    change to general format

--- Firmware/Balancing_Robot/Src/main.c
+++ Firmware/Balancing_Robot/Src/main.c
@@ -239,7 +239,10 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	one_ms_timer_interrupt();
+	if (htim->Instance == htim1.Instance)
+	{
+		one_ms_timer_interrupt();
+	}
 }
 /* USER CODE END 4 */
 
